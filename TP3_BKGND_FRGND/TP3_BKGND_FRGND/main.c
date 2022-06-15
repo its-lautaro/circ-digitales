de745df13bc68e3ca5aee314e6fd4ceb1c201988
Adrián Daniel Barral <82920560+elcuartodan@users.noreply.github.com>
2022-06-14 23:22:53 -0300

    Initial commit
    This is the initial commit of a fancy project that came out of the blue for no apparent reason.

---
+++ TP3_BKGND_FRGND/TP3_BKGND_FRGND/main.c
@@ -0,0 +1,18 @@
+/*
+ * TP3_BKGND_FRGND.c
+ *
+ * Created: 14/6/2022 23:06:58
+ * Author : Adrian
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
