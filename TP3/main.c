94dce6e3201bfe57c7c6b0e9f650321866bca52c
Julia Gonik <juligonik@gmail.com>
2022-06-23 21:24:13 -0300

    add TP3

---
+++ TP3/main.c
@@ -0,0 +1,18 @@
+/*
+ * TP3.c
+ *
+ * Created: 23/6/2022 21:17:17
+ * Author : USUARIO
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
