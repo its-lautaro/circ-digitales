7ca32a127d86f70d3d196f63af2d814e9f06e818
Julia Gonik <juligonik@gmail.com>
2022-06-26 16:29:32 -0300

    SERIAL PORT

--- TP3/uart.h
+++ TP3/uart.h
@@ -10,4 +10,13 @@ void UART_TransmitPolling(uint8_t DataByte);
 
 void UART_TransmitString(char *);
 
+uint8_t UART_HayComando();
+void UART_Set_False_Command_Flag();
+char *UART_GetComando();
+void UART_Set_Frecuencia(int f);
+void UART_On();
+void UART_Off();
+void UART_Reset();
+void UART_Print_Error();
+
 #endif
